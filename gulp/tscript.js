25714d36c9237f4cb59c493d8e4808e3c9581011
THBar <choreno@gmail.com>
2016-09-29 16:40:13 -0400

    added typescript task

---
+++ gulp/tscript.js
@@ -0,0 +1,32 @@
+
+//use the tsconfig.json
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject('tsconfig.json'); 
+
+gulp.task('default', function(){
+
+
+    return tsProject.src()
+    .pipe(tsProject())
+    .js
+    .pipe(gulp.dest('dist')); 
+})
+
+
+
+// //no use of tsconfig.json
+// var gulp = require('gulp');
+// var ts = require('gulp-typescript');
+
+// var gulp = require('gulp');
+// var ts = require('gulp-typescript');
+ 
+// gulp.task('default', function () {
+//     return gulp.src('src/**/*.ts')
+//         .pipe(ts({
+//             noImplicitAny: true,
+//             out: 'output.js'
+//         }))
+//         .pipe(gulp.dest('built/local'));
+// });
